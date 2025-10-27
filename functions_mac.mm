ab5ac6cd981899a5521cae4eff6b56d33813c0f8
Ashu <ashu.bashir@anywhere.co>
2025-10-27 12:21:06 +0530

    Fix SIGSEGV crash with safe property-based approach

--- functions_mac.mm
+++ functions_mac.mm
@@ -5,71 +5,20 @@
 #import <objc/runtime.h>
 #include "functions.h"
 
-// Key for storing swizzled state - moved up here for better organization
+// Using simple property setting approach - much safer than method swizzling or class swapping
 
-// Instead of class swapping, we'll use method swizzling to safely override behavior
-@interface NSWindow (PanelAdditions)
-- (NSWindowStyleMask)panel_styleMask;
-- (NSWindowCollectionBehavior)panel_collectionBehavior;
-- (BOOL)panel_isFloatingPanel;
-- (NSWindowLevel)panel_level;
-- (BOOL)panel_canBecomeKeyWindow;
-- (BOOL)panel_canBecomeMainWindow;
-- (BOOL)panel_needsPanelToBecomeKey;
-- (BOOL)panel_acceptsFirstResponder;
+// Simple and safe approach - just set window properties directly
+// Key for storing original properties
+static const void *kOriginalPropertiesKey = &kOriginalPropertiesKey;
+
+@interface WindowProperties : NSObject
+@property (nonatomic) NSWindowStyleMask originalStyleMask;
+@property (nonatomic) NSWindowCollectionBehavior originalCollectionBehavior;
+@property (nonatomic) NSWindowLevel originalLevel;
 @end
 
-@implementation NSWindow (PanelAdditions)
-- (NSWindowStyleMask)panel_styleMask {
-  return NSWindowStyleMaskTexturedBackground | NSWindowStyleMaskResizable | NSWindowStyleMaskFullSizeContentView | NSWindowStyleMaskNonactivatingPanel;
-}
-- (NSWindowCollectionBehavior)panel_collectionBehavior {
-  return NSWindowCollectionBehaviorCanJoinAllSpaces | NSWindowCollectionBehaviorFullScreenAuxiliary;
-}
-- (BOOL)panel_isFloatingPanel {
-  return YES;
-}
-- (NSWindowLevel)panel_level {
-  return NSFloatingWindowLevel;
-}
-- (BOOL)panel_canBecomeKeyWindow {
-  return YES;
-}
-- (BOOL)panel_canBecomeMainWindow {
-  return YES;
-}
-- (BOOL)panel_needsPanelToBecomeKey {
-  return YES;
-}
-- (BOOL)panel_acceptsFirstResponder {
-  return YES;
-}
+@implementation WindowProperties
 @end
-
-// We no longer need the PROPanel class since we're using method swizzling
-// This is much safer than runtime class swapping
-
-// Helper function to swizzle methods safely
-void swizzleMethod(Class cls, SEL originalSelector, SEL swizzledSelector) {
-  Method originalMethod = class_getInstanceMethod(cls, originalSelector);
-  Method swizzledMethod = class_getInstanceMethod(cls, swizzledSelector);
-  
-  if (!originalMethod || !swizzledMethod) {
-    NSLog(@"Failed to swizzle method %@ on class %@", NSStringFromSelector(originalSelector), NSStringFromClass(cls));
-    return;
-  }
-  
-  BOOL didAddMethod = class_addMethod(cls, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-  
-  if (didAddMethod) {
-    class_replaceMethod(cls, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-  } else {
-    method_exchangeImplementations(originalMethod, swizzledMethod);
-  }
-}
-
-// Key for storing swizzled state
-static const void *kPanelSwizzledKey = &kPanelSwizzledKey;
 
 NAN_METHOD(MakePanel) {
   v8::Local<v8::Object> handleBuffer = info[0].As<v8::Object>();
@@ -84,35 +33,33 @@ NAN_METHOD(MakePanel) {
 
   NSWindow *nswindow = [mainContentView window];
   
-  // Check if already swizzled
-  id swizzled = objc_getAssociatedObject(nswindow, kPanelSwizzledKey);
-  if (swizzled) {
+  // Check if already converted
+  id stored = objc_getAssociatedObject(nswindow, kOriginalPropertiesKey);
+  if (stored) {
     NSLog(@"MakePanel: Window %p already converted to panel", nswindow);
     return info.GetReturnValue().Set(true);
   }
   
-  NSLog(@"MakePanel: Converting window %p to panel using method swizzling", nswindow);
+  NSLog(@"MakePanel: Converting window %p to panel by setting properties", nswindow);
 
+  // Store original properties
+  WindowProperties *originalProps = [[WindowProperties alloc] init];
+  originalProps.originalStyleMask = nswindow.styleMask;
+  originalProps.originalCollectionBehavior = nswindow.collectionBehavior;
+  originalProps.originalLevel = nswindow.level;
+  
+  objc_setAssociatedObject(nswindow, kOriginalPropertiesKey, originalProps, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+  // Set panel properties directly - much safer than method swizzling
   nswindow.titlebarAppearsTransparent = true;
   nswindow.titleVisibility = (NSWindowTitleVisibility)1;
-
-  // Use method swizzling instead of class swapping - much safer
-  Class windowClass = [nswindow class];
   
-  // Swizzle the methods to panel behavior
-  swizzleMethod(windowClass, @selector(styleMask), @selector(panel_styleMask));
-  swizzleMethod(windowClass, @selector(collectionBehavior), @selector(panel_collectionBehavior));
-  swizzleMethod(windowClass, @selector(isFloatingPanel), @selector(panel_isFloatingPanel));
-  swizzleMethod(windowClass, @selector(level), @selector(panel_level));
-  swizzleMethod(windowClass, @selector(canBecomeKeyWindow), @selector(panel_canBecomeKeyWindow));
-  swizzleMethod(windowClass, @selector(canBecomeMainWindow), @selector(panel_canBecomeMainWindow));
-  swizzleMethod(windowClass, @selector(needsPanelToBecomeKey), @selector(panel_needsPanelToBecomeKey));
-  swizzleMethod(windowClass, @selector(acceptsFirstResponder), @selector(panel_acceptsFirstResponder));
+  // Set panel-like behavior
+  nswindow.styleMask = NSWindowStyleMaskTexturedBackground | NSWindowStyleMaskResizable | NSWindowStyleMaskFullSizeContentView | NSWindowStyleMaskNonactivatingPanel;
+  nswindow.collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces | NSWindowCollectionBehaviorFullScreenAuxiliary;
+  nswindow.level = NSFloatingWindowLevel;
   
-  // Mark as swizzled
-  objc_setAssociatedObject(nswindow, kPanelSwizzledKey, @YES, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-  
-  NSLog(@"MakePanel: Successfully swizzled window %p methods", nswindow);
+  NSLog(@"MakePanel: Successfully converted window %p to panel", nswindow);
 
   return info.GetReturnValue().Set(true);
 }
@@ -147,31 +94,24 @@ NAN_METHOD(MakeWindow) {
 
   NSWindow* newWindow = mainContentView.window;
 
-  // Check if swizzled
-  id swizzled = objc_getAssociatedObject(newWindow, kPanelSwizzledKey);
-  if (!swizzled) {
-    NSLog(@"MakeWindow: Window %p was not swizzled", newWindow);
+  // Check if we have stored properties
+  WindowProperties *originalProps = objc_getAssociatedObject(newWindow, kOriginalPropertiesKey);
+  if (!originalProps) {
+    NSLog(@"MakeWindow: Window %p was not converted to panel", newWindow);
     return info.GetReturnValue().Set(true);
   }
 
-  NSLog(@"MakeWindow: Restoring window %p from panel behavior", newWindow);
+  NSLog(@"MakeWindow: Restoring window %p from panel to original window behavior", newWindow);
 
-  Class windowClass = [newWindow class];
+  // Restore original properties
+  newWindow.styleMask = originalProps.originalStyleMask;
+  newWindow.collectionBehavior = originalProps.originalCollectionBehavior;
+  newWindow.level = originalProps.originalLevel;
   
-  // Unswizzle the methods back to original behavior
-  swizzleMethod(windowClass, @selector(styleMask), @selector(panel_styleMask));
-  swizzleMethod(windowClass, @selector(collectionBehavior), @selector(panel_collectionBehavior));
-  swizzleMethod(windowClass, @selector(isFloatingPanel), @selector(panel_isFloatingPanel));
-  swizzleMethod(windowClass, @selector(level), @selector(panel_level));
-  swizzleMethod(windowClass, @selector(canBecomeKeyWindow), @selector(panel_canBecomeKeyWindow));
-  swizzleMethod(windowClass, @selector(canBecomeMainWindow), @selector(panel_canBecomeMainWindow));
-  swizzleMethod(windowClass, @selector(needsPanelToBecomeKey), @selector(panel_needsPanelToBecomeKey));
-  swizzleMethod(windowClass, @selector(acceptsFirstResponder), @selector(panel_acceptsFirstResponder));
+  // Clear the stored properties
+  objc_setAssociatedObject(newWindow, kOriginalPropertiesKey, nil, OBJC_ASSOCIATION_ASSIGN);
   
-  // Clear the swizzled marker
-  objc_setAssociatedObject(newWindow, kPanelSwizzledKey, nil, OBJC_ASSOCIATION_ASSIGN);
-  
-  NSLog(@"MakeWindow: Successfully restored window %p methods", newWindow);
+  NSLog(@"MakeWindow: Successfully restored window %p to original behavior", newWindow);
 
   return info.GetReturnValue().Set(true);
 }
