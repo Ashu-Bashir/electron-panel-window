5cf2bcfb89fd7fb9ee07379ebf587d6837a64b70
Ashu <ashu.bashir@anywhere.co>
2025-10-28 15:45:33 +0530

    fix: retore to the working version

--- functions_mac.mm
+++ functions_mac.mm
@@ -2,23 +2,149 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 #import <objc/objc-runtime.h>
-#import <objc/runtime.h>
 #include "functions.h"
 
-// Using simple property setting approach - much safer than method swizzling or class swapping
-
-// Simple and safe approach - just set window properties directly
-// Key for storing original properties
-static const void *kOriginalPropertiesKey = &kOriginalPropertiesKey;
-
-@interface WindowProperties : NSObject
-@property (nonatomic) NSWindowStyleMask originalStyleMask;
-@property (nonatomic) NSWindowCollectionBehavior originalCollectionBehavior;
-@property (nonatomic) NSWindowLevel originalLevel;
+@interface PROPanel : NSWindow
 @end
 
-@implementation WindowProperties
+@implementation PROPanel
+- (NSWindowStyleMask)styleMask {
+  return NSWindowStyleMaskTexturedBackground | NSWindowStyleMaskResizable | NSWindowStyleMaskFullSizeContentView | NSWindowStyleMaskNonactivatingPanel;
+}
+- (NSWindowCollectionBehavior)collectionBehavior {
+  return NSWindowCollectionBehaviorCanJoinAllSpaces | NSWindowCollectionBehaviorFullScreenAuxiliary;
+}
+- (BOOL)isFloatingPanel {
+  return YES;
+}
+- (NSWindowLevel)level {
+  return NSFloatingWindowLevel;
+}
+- (BOOL)canBecomeKeyWindow {
+  return YES;
+}
+- (BOOL)canBecomeMainWindow {
+  return YES;
+}
+- (BOOL)needsPanelToBecomeKey {
+  return YES;
+}
+- (BOOL)acceptsFirstResponder {
+  return YES;
+}
+- (void)removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath context:(nullable void *)context {
+  // macOS Big Sur attempts to remove an observer for the NSTitlebarView that doesn't exist.
+  // This is due to us changing the class from NSWindow -> NSPanel at runtime, it's possible
+  // there is assumed setup that doesn't happen. Details of the exception this is avoiding are
+  // here: https://github.com/goabstract/electron-panel-window/issues/6
+  if ([keyPath isEqualToString:@"_titlebarBackdropGroupName"]) {
+    // NSLog(@"removeObserver ignored");
+    return;
+  }
+
+  if (context) {
+    [super removeObserver:observer forKeyPath:keyPath context:context];
+  } else {
+    [super removeObserver:observer forKeyPath:keyPath];
+  }
+}
+- (void)removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath {
+  [self removeObserver:observer forKeyPath:keyPath context:NULL];
+}
+- (void)disableHeadlessMode {
+  // Electron 36+ compatibility - no-op for panel windows
+  // Headless mode is typically used for testing/automation, 
+  // which doesn't apply to panel windows
+}
+- (void)cleanup {
+  // Prevent crash when Electron tries to call cleanup on panel windows
+  // This method is expected by newer versions of Electron but doesn't exist
+  // on NSPanel, so we provide a no-op implementation
+  NSLog(@"PROPanel cleanup called - no-op to prevent crash");
+}
+
+// Additional cleanup methods that Electron might expect
+- (void)cleanupWebContents {
+  // No-op to prevent crashes
+  NSLog(@"PROPanel cleanupWebContents called - no-op");
+}
+
+- (void)cleanupBrowserWindow {
+  // No-op to prevent crashes  
+  NSLog(@"PROPanel cleanupBrowserWindow called - no-op");
+}
+
+- (void)destroy {
+  // Another method Electron might call during destruction
+  NSLog(@"PROPanel destroy called - no-op");
+}
+
+- (void)_destroy {
+  // Private destroy method Electron might call
+  NSLog(@"PROPanel _destroy called - no-op");
+}
+
+// Additional Electron window methods that might be called
+- (void)closeWebContents {
+  NSLog(@"PROPanel closeWebContents called - no-op");
+}
+
+- (void)destroyWebContents {
+  NSLog(@"PROPanel destroyWebContents called - no-op");
+}
+
+- (void)_closeWebContents {
+  NSLog(@"PROPanel _closeWebContents called - no-op");
+}
+
+- (void)handleWindowClose {
+  NSLog(@"PROPanel handleWindowClose called - no-op");
+}
+
+- (void)willClose {
+  NSLog(@"PROPanel willClose called - no-op");
+}
+
+- (void)_willClose {
+  NSLog(@"PROPanel _willClose called - no-op");
+}
+
+// Override dealloc to log and safely clean up
+- (void)dealloc {
+  NSLog(@"PROPanel dealloc called");
+  [super dealloc];
+}
+
+// Forward any unknown method calls to prevent crashes
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+  NSMethodSignature *signature = [super methodSignatureForSelector:aSelector];
+  if (!signature) {
+    NSLog(@"PROPanel: Creating dummy signature for missing method %@", NSStringFromSelector(aSelector));
+    // Create a dummy signature for any missing selector
+    // This signature represents: void method(id self, SEL _cmd)
+    signature = [NSMethodSignature signatureWithObjCTypes:"v@:"];
+  }
+  return signature;
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+  // Log the missing method and do nothing
+  NSLog(@"PROPanel: Missing method %@ called - ignoring to prevent crash", NSStringFromSelector([anInvocation selector]));
+  // Don't call anything - just return safely
+}
+
+// Override respondsToSelector to claim we can handle any selector
+- (BOOL)respondsToSelector:(SEL)aSelector {
+  BOOL responds = [super respondsToSelector:aSelector];
+  if (!responds) {
+    NSLog(@"PROPanel: Claiming to respond to missing selector %@ to prevent crash", NSStringFromSelector(aSelector));
+    return YES; // Claim we can handle it, then forward to forwardInvocation
+  }
+  return responds;
+}
 @end
+
+Class electronWindowClass;
 
 NAN_METHOD(MakePanel) {
   v8::Local<v8::Object> handleBuffer = info[0].As<v8::Object>();
@@ -31,35 +157,23 @@ NAN_METHOD(MakePanel) {
   if (!mainContentView)
       return info.GetReturnValue().Set(false);
 
+  electronWindowClass = [mainContentView.window class];
+
+//   NSLog(@"class of main window before = %@", object_getClass(mainContentView.window));
+
   NSWindow *nswindow = [mainContentView window];
-  
-  // Check if already converted
-  id stored = objc_getAssociatedObject(nswindow, kOriginalPropertiesKey);
-  if (stored) {
-    NSLog(@"MakePanel: Window %p already converted to panel", nswindow);
-    return info.GetReturnValue().Set(true);
-  }
-  
-  NSLog(@"MakePanel: Converting window %p to panel by setting properties", nswindow);
-
-  // Store original properties
-  WindowProperties *originalProps = [[WindowProperties alloc] init];
-  originalProps.originalStyleMask = nswindow.styleMask;
-  originalProps.originalCollectionBehavior = nswindow.collectionBehavior;
-  originalProps.originalLevel = nswindow.level;
-  
-  objc_setAssociatedObject(nswindow, kOriginalPropertiesKey, originalProps, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-
-  // Set panel properties directly - much safer than method swizzling
   nswindow.titlebarAppearsTransparent = true;
   nswindow.titleVisibility = (NSWindowTitleVisibility)1;
-  
-  // Set panel-like behavior
-  nswindow.styleMask = NSWindowStyleMaskTexturedBackground | NSWindowStyleMaskResizable | NSWindowStyleMaskFullSizeContentView | NSWindowStyleMaskNonactivatingPanel;
-  nswindow.collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces | NSWindowCollectionBehaviorFullScreenAuxiliary;
-  nswindow.level = NSFloatingWindowLevel;
-  
-  NSLog(@"MakePanel: Successfully converted window %p to panel", nswindow);
+
+//   NSLog(@"stylemask = %ld", mainContentView.window.styleMask);
+
+  // Convert the NSWindow class to PROPanel
+  object_setClass(mainContentView.window, [PROPanel class]);
+
+//   NSLog(@"class of main window after = %@", object_getClass(mainContentView.window));
+//   NSLog(@"stylemask after = %ld", mainContentView.window.styleMask);
+
+
 
   return info.GetReturnValue().Set(true);
 }
@@ -82,6 +196,7 @@ NAN_METHOD(MakeKeyWindow) {
 
 
 NAN_METHOD(MakeWindow) {
+
   v8::Local<v8::Object> handleBuffer = info[0].As<v8::Object>();
   v8::Isolate* isolate = info.GetIsolate();
   v8::HandleScope scope(isolate);
@@ -89,29 +204,13 @@ NAN_METHOD(MakeWindow) {
   char* buffer = node::Buffer::Data(handleBuffer);
   NSView* mainContentView = *reinterpret_cast<NSView**>(buffer);
 
-  if (!mainContentView)
+    if (!mainContentView)
       return info.GetReturnValue().Set(false);
 
   NSWindow* newWindow = mainContentView.window;
 
-  // Check if we have stored properties
-  WindowProperties *originalProps = objc_getAssociatedObject(newWindow, kOriginalPropertiesKey);
-  if (!originalProps) {
-    NSLog(@"MakeWindow: Window %p was not converted to panel", newWindow);
-    return info.GetReturnValue().Set(true);
-  }
-
-  NSLog(@"MakeWindow: Restoring window %p from panel to original window behavior", newWindow);
-
-  // Restore original properties
-  newWindow.styleMask = originalProps.originalStyleMask;
-  newWindow.collectionBehavior = originalProps.originalCollectionBehavior;
-  newWindow.level = originalProps.originalLevel;
-  
-  // Clear the stored properties
-  objc_setAssociatedObject(newWindow, kOriginalPropertiesKey, nil, OBJC_ASSOCIATION_ASSIGN);
-  
-  NSLog(@"MakeWindow: Successfully restored window %p to original behavior", newWindow);
+  // Convert the NSPanel class to whatever it was before
+  object_setClass(newWindow, electronWindowClass);
 
   return info.GetReturnValue().Set(true);
 }
